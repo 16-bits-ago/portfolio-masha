64f32703b5102f5099a85b712a544876b139c297
Vlad Kapustsin <kapustsin.u@titan-t.com>
2023-02-12 00:27:07 +0200

    fix back

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
 <React.StrictMode>
-  <BrowserRouter basename='/'>
+  <BrowserRouter basename='/portfolio-masha'>
     <App/>
   </BrowserRouter>
 </React.StrictMode>
