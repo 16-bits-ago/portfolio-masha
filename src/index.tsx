00dcb016d73d32a214f775b05603f9b00f54c654
Vlad Kapustsin <kapustsin.u@titan-t.com>
2023-02-12 00:13:01 +0200

    fix refresh page GH pages

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
 <React.StrictMode>
-  <BrowserRouter>
+  <BrowserRouter basename='/portfolio-masha'>
     <App/>
   </BrowserRouter>
 </React.StrictMode>
--- src/router/HeaderRouter.tsx
+++ src/router/HeaderRouter.tsx
@@ -1,7 +1,6 @@
 import { Routes, Route } from "react-router-dom"
 import About from "../components/About"
 import Home from "../components/Home"
-import NoPage from "../components/NoPage"
 import Layout from "../components/layout/Layout"
 import ZernoEcomers from "../components/zerno/ZernoEcomersLayout"
 import ProjectorLayout from "../components/projector/ProjectorLayout"
